1567a7cb1a616d61b1ef91bb9161cff084738898
jormarcus <jormarcus92@gmail.com>
2023-11-09 14:13:32 -0500

    Fix dark/light mode and website link

--- src/components/theme-switch.tsx
+++ src/components/theme-switch.tsx
@@ -2,7 +2,7 @@
 
 import { useTheme } from 'next-themes';
 import { useEffect, useState } from 'react';
-import { BsMoon, BsSun } from 'react-icons/bs';
+import { BsFillSunFill, BsMoon, BsMoonStarsFill, BsSun } from 'react-icons/bs';
 
 const ThemeSwitch = () => {
   const [mounted, setMounted] = useState(false);
@@ -23,7 +23,11 @@ const ThemeSwitch = () => {
       className="fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950"
       onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
     >
-      {theme === 'light' ? <BsSun /> : <BsMoon />}
+      {theme === 'light' ? (
+        <BsMoonStarsFill size={27} />
+      ) : (
+        <BsFillSunFill size={27} />
+      )}
     </button>
   );
 };
